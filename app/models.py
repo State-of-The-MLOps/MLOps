425a9f756915b60cf4e1aa460b011cd6c8258f27
ehddnr301 <dy950328@gmail.com>
2021-09-06 16:31:39 +0900

    Modify Table structure
    ML model과 metadata정보를 구분하여 테이블을 생성하였습니다.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import datetime
-from sqlalchemy import Column, Integer, String, FLOAT, DateTime
+from sqlalchemy import Column, Integer, String, FLOAT, DateTime, ForeignKey
 from sqlalchemy.sql.functions import now
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -37,12 +38,21 @@ class ClfModel(Base):
 class RegModel(Base):
     __tablename__ = 'reg_model'
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    model_name = Column(String, primary_key=True)
     path = Column(String, nullable=False)
+
+    model_metadata = relationship(
+        "reg_model_metadata", backref="reg_model.model_name")
+
+
+class RegModelMetadata(Base):
+    __tablename__ = 'reg_model_metadata'
+
+    experiment_name = Column(String, primary_key=True)
+    reg_model_name = Column(String, ForeignKey(
+        'reg_model.model_name'), nullable=False)
     experimenter = Column(String, nullable=False)
-    experiment_name = Column(String, nullable=False)
-    model_name = Column(String, nullable=False)
-    version = Column(FLOAT, autoincrement=True)
+    version = Column(FLOAT)
     train_mae = Column(FLOAT, nullable=False)
     val_mae = Column(FLOAT, nullable=False)
     train_mse = Column(FLOAT, nullable=False)
