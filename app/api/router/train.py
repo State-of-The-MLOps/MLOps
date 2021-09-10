a28ee89266aaabd54301b677701e88861a6d6a2a
ehddnr301 <dy950328@gmail.com>
2021-09-10 13:17:33 +0900

    Add training API
    training 하는것을 API로 만들어서 요청을 받습니다.
    요청받은 값으로 config.yml 을 만들고 그 파일로 training을 진행합니다.

---
+++ app/api/router/train.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from app.utils import write_yml
+import subprocess
+
+from fastapi import APIRouter
+
+
+router = APIRouter(
+    prefix="/train",
+    tags=["train"],
+    responses={404: {"description": "Not Found"}}
+)
+
+
+@router.put("/")
+def train_insurance(
+    PORT: int = 8080,
+    experiment_sec: int = 20,
+    experiment_name: str = 'exp1',
+    experimenter: str = 'DongUk',
+    model_name: str = 'insurance_fee_model',
+    version: float = 0.1
+):
+    """
+    Args:
+        PORT (int): PORT to run NNi. Defaults to 8080
+        experiment_sec (int): Express the experiment time in seconds Defaults to 20
+        experiment_name (str): experiment name Defaults to exp1
+        experimeter (str): experimenter (author) Defaults to DongUk
+        model_name (str): model name Defaults to insurance_fee_model
+        version (float): version of experiment Defaults to 0.1
+    Returns:
+        msg: Regardless of success or not, return address values including PORT.
+    """
+    path = 'experiments/insurance/'
+    try:
+        write_yml(
+            path,
+            experiment_name,
+            experimenter,
+            model_name,
+            version
+        )
+        subprocess.Popen(
+            "nnictl create --port {} --config {}/{}.yml && timeout {} && nnictl stop --port {}".format(
+                PORT, path, model_name, experiment_sec, PORT),
+            shell=True,
+        )
+
+    except Exception as e:
+        print('error')
+        print(e)
+
+    return {"msg": f'Check out http://127.0.0.1:{PORT}'}
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from app.api.router import predict
+from app.api.router import predict, train
 
 app = FastAPI()
 
@@ -17,15 +17,17 @@ app.add_middleware(
 )
 
 app.include_router(predict.router)
+app.include_router(train.router)
 
 
 @app.get("/")
 def hello_world():
     return {"message": "Hello World"}
 
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", 
-                host="0.0.0.0", 
-                port=8000, 
-                reload=True, 
+    uvicorn.run("main:app",
+                host="0.0.0.0",
+                port=8000,
+                reload=True,
                 reload_dirs=['app/'])
