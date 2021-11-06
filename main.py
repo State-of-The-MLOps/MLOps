6f189c81530c9bcaa931f407e0270bde17b4de45
hlchoi <hl8469@gmail.com>
2021-11-06 08:00:52 +0000

    Delete experiments and import train code 1. 현재 사용하지 않는 experiments 폴더를 삭제하였습니다. 2. main.py에서 train api를 import하여 에러가 나던 부분을 수정하였습니다.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.api.router import predict, train
+from app.api.router import predict
 
 app = FastAPI()
 
@@ -17,7 +17,6 @@ app.add_middleware(
 )
 
 app.include_router(predict.router)
-app.include_router(train.router)
 
 
 @app.get("/")
