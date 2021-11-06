2e69cb58ce2c3d79a28f41033d309c031d768525
ehddnr301 <dy950328@gmail.com>
2021-11-06 16:06:44 +0900

    Add deprecated folder
    deprecated 된 것들은 폴더에 모아서 기록하였습니다.

---
+++ deprecated/experiments/expr_db.py
@@ -0,0 +1,22 @@
+import os
+
+import sqlalchemy
+from dotenv import load_dotenv
+
+
+def connect(db="postgres"):
+    """Returns a connection and a metadata object"""
+
+    load_dotenv(verbose=True)
+
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER = os.getenv("POSTGRES_SERVER")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+    POSTGRES_DB = db
+
+    url = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+    connection = sqlalchemy.create_engine(url)
+
+    return connection
---
+++ deprecated/experiments/insurance/trial.py
@@ -0,0 +1,167 @@
+import codecs
+import getopt
+import os
+import pickle
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+import nni
+import numpy as np
+import pandas as pd
+from dotenv import load_dotenv
+from expr_db import connect
+from query import *
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import KFold, train_test_split
+from sklearn.preprocessing import LabelEncoder
+from xgboost.sklearn import XGBRegressor
+
+load_dotenv(verbose=True)
+
+
+def preprocess(x_train, x_valid, col_list):
+    """
+    param:
+        x_train : train dataset dataframe
+        x_valid : validation dataset dataframe
+        col_list : columns that required for LabelEncoding
+    return:
+        tmp_x_train.values : numpy.ndarray
+        tmp_x_valid.values : numpy.ndarray
+    """
+    tmp_x_train = x_train.copy()
+    tmp_x_valid = x_valid.copy()
+
+    tmp_x_train.reset_index(drop=True, inplace=True)
+    tmp_x_valid.reset_index(drop=True, inplace=True)
+
+    encoder = LabelEncoder()
+
+    for col in col_list:
+        tmp_x_train.loc[:, col] = encoder.fit_transform(
+            tmp_x_train.loc[:, col]
+        )
+        tmp_x_valid.loc[:, col] = encoder.transform(tmp_x_valid.loc[:, col])
+
+    return tmp_x_train.values, tmp_x_valid.values
+
+
+def main(params, engine, experiment_info, connection):
+    """
+    param:
+        params: Parameters determined by NNi
+        engine: sqlalchemy engine
+        experiment_info: information of experiment [dict]
+        connection: connection used to communicate with DB
+    """
+
+    df = pd.read_sql(SELECT_ALL_INSURANCE, engine)
+    experimenter = experiment_info["experimenter"]
+    experiment_name = experiment_info["experiment_name"]
+    model_name = experiment_info["model_name"]
+    version = experiment_info["version"]
+
+    label_col = ["sex", "smoker", "region"]
+
+    y = df.charges.to_frame()
+    x = df.iloc[:, :-1]
+
+    x_train, x_valid, y_train, y_valid = train_test_split(
+        x, y, test_size=0.2, random_state=42
+    )
+
+    kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+    cv_mse, cv_mae, tr_mse, tr_mae = [], [], [], []
+    fold_mae, fold_model = 1e10, None
+
+    for trn_idx, val_idx in kf.split(x, y):
+        x_train, y_train = x.iloc[trn_idx], y.iloc[trn_idx]
+        x_valid, y_valid = x.iloc[val_idx], y.iloc[val_idx]
+
+        # 전처리
+        x_tra, x_val = preprocess(x_train, x_valid, label_col)
+
+        # 모델 정의 및 파라미터 전달
+        model = XGBRegressor(**params)
+
+        # 모델 학습 및 Early Stopping 적용
+        model.fit(
+            x_tra,
+            y_train,
+            eval_set=[(x_val, y_valid)],
+            early_stopping_rounds=10,
+        )
+
+        y_train_pred = model.predict(x_tra)
+        y_valid_pred = model.predict(x_val)
+        # Loss 계산
+        train_mse = mean_squared_error(y_train, y_train_pred)
+        valid_mse = mean_squared_error(y_valid, y_valid_pred)
+        train_mae = mean_absolute_error(y_train, y_train_pred)
+        valid_mae = mean_absolute_error(y_valid, y_valid_pred)
+
+        cv_mse.append(valid_mse)
+        cv_mae.append(valid_mae)
+        tr_mse.append(train_mse)
+        tr_mae.append(train_mae)
+
+        new_mae = min(fold_mae, valid_mae)
+        if new_mae != fold_mae:
+            fold_model = model
+
+    cv_mse_mean = np.mean(cv_mse)
+    cv_mae_mean = np.mean(cv_mae)
+    tr_mse_mean = np.mean(tr_mse)
+    tr_mae_mean = np.mean(tr_mae)
+    pickled_model = codecs.encode(pickle.dumps(fold_model), "base64").decode()
+
+    connection.execute(
+        INSERT_TEMP_MODEL.format(
+            model_name,
+            pickled_model,
+            experiment_name,
+            experimenter,
+            version,
+            tr_mae_mean,
+            cv_mae_mean,
+            tr_mse_mean,
+            cv_mse_mean,
+        )
+    )
+
+    nni.report_final_result(cv_mae_mean)
+    print("Final result is %g", cv_mae_mean)
+    print("Send final result done.")
+
+
+if __name__ == "__main__":
+    params = nni.get_next_parameter()
+    engine = connect()
+    argv = sys.argv
+    experiment_info = {}
+
+    try:
+        opts, etc_args = getopt.getopt(
+            argv[1:],
+            "e:n:m:v:",
+            ["experimenter=", "experiment_name=", "model_name=", "version="],
+        )
+        for opt, arg in opts:
+            if opt in ("-e", "--experimenter"):
+                experiment_info["experimenter"] = f"'{arg}'"
+            elif opt in ("-n", "--experiment_name"):
+                experiment_info["experiment_name"] = f"'{arg}'"
+            elif opt in ("-m", "--model_name"):
+                experiment_info["model_name"] = f"'{arg}'"
+            elif opt in ("-v", "--version"):
+                experiment_info["version"] = arg
+
+    except getopt.GetoptError:
+        sys.exit(2)
+
+    with engine.connect() as connection:
+        with connection.begin():
+            main(params, engine, experiment_info, connection)
